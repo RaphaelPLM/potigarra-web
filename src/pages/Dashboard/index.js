85c1210cc557b66c5cdb6c240a5173f5180711f6
Raphael Müller <raphaelplmuller@icloud.com>
2020-04-20 17:37:37 -0300

    Add first version of dashboard layout

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React from "react";
+import "./styles.css";
 
 export default function Dashboard() {
-	return <h1>Dashboard</h1>;
+  return (
+    <div className="grid-container">
+      <header className="header"></header>
+      <aside className="sidenav">
+        <ul className="list">
+          <li className="item">Item One</li>
+          <li className="item">Item Two</li>
+          <li className="item">Item Three</li>
+          <li className="item">Item Four</li>
+          <li className="item">Item Five</li>
+        </ul>
+      </aside>
+      <main className="main">
+        <ul className="menu">
+          <li className="item dark-gray"><h4>Pendentes</h4></li>
+          <li className="item dark-gray"><h4>Ativos</h4></li>
+          <li className="item dark-gray"><h4>Expirados</h4></li>
+          <li className="item dark-gray"><h4>Todos</h4></li>
+        </ul>
+        <div className="body">
+          <h3 className="dark-gray">Cartões pendentes</h3>
+          <div className="card-grid">
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+            <div className="card">Card</div>
+          </div>
+        </div>
+      </main>
+      <footer className="footer"></footer>
+    </div>
+  );
 }
