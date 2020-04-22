38f72976ab74a82a2d4cd085e80133dce307ada0
Raphael Müller <raphaelplmuller@icloud.com>
2020-04-22 17:27:54 -0300

    Add filter based on card status

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,55 +1,140 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import "./styles.css";
 import profilePicture from "../../assets/download.jpg";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { IconButton } from "@material-ui/core";
+import api from "../../services/api";
 
-export default function Card() {
-  return (
-    <ul className="card">
-      <header className="card-header">
-        <h5>Status</h5>
-        <div className="card-status"></div>
-      </header>
-      <body className="card-body">
+export default function Card(props) {
+  const [cardFront, setCardFront] = useState(true);
+
+  const backgroundColor = setFooterColor();
+
+  function setFooterColor() {
+    if (props.card.status === "Pending") {
+      return { backgroundColor: "#ff7e57" };
+    } else if (props.card.status === "Active") {
+      return { backgroundColor: "rgb(3, 199, 140)" };
+    } else if (props.card.status === "Expired") {
+      return { backgroundColor: "#ff2d5b" };
+    }
+  }
+
+  function formatDate(unformattedDate) {
+    const date = new Date(unformattedDate);
+
+    return date.toLocaleDateString("en-GB");
+  }
+
+  async function activateMember() {
+    let token = localStorage.getItem("@potigarra/token");
+
+    api
+      .put(
+        "/cards/generate",
+        { memberId: props.card.member_id },
+        { headers: { Authorization: "Bearer " + token } }
+      )
+      .then((response) => {
+        console.log("SUCCESS", response.data);
+      })
+      .catch((error) => console.log("ERROR", error));
+  }
+
+  function CardFront() {
+    return (
+      <Fragment>
         <img className="card-image" src={profilePicture} />
-        <div className="double-field">
-          <h5 className="dark-gray">Nome</h5>
-          <h5 className="blue">Raphael Paula Leite Müller</h5>
-        </div>
-        <div className="field">
-          <h5 className="dark-gray">CPF</h5>
-          <h5 className="blue">123.456.789-00</h5>
-        </div>
 
-        <div className="field">
-          <h5 className="dark-gray">RG</h5>
-          <h5 className="blue">1.123.123</h5>
-        </div>
+        <body className="card-body">
+          <div>
+            <h4 className="blue">{props.card.username}</h4>
+          </div>
 
-        <div className="field">
-          <h5 className="dark-gray">Gênero</h5>
-          <h5 className="blue">Masculino</h5>
-        </div>
+          <br />
 
-        <div className="field">
-          <h5 className="dark-gray">Data de nasc.</h5>
-          <h5 className="blue">25/11/1999</h5>
-        </div>
+          <div>
+            <h5 className="dark-gray">Data de registro</h5>
+            <h5 className="blue">{formatDate(props.card.created_at)}</h5>
+          </div>
+        </body>
 
-        <div className="field">
-          <h5 className="dark-gray">Turma</h5>
-          <h5 className="blue">112</h5>
-        </div>
+        <footer style={backgroundColor} className="card-footer"></footer>
+      </Fragment>
+    );
+  }
 
-        <div className="field">
-          <h5 className="dark-gray">Dt. associação</h5>
-          <h5 className="blue">01/01/2020</h5>
-        </div>
+  function CardBack() {
+    return (
+      <Fragment>
+        <body className="card-back-body">
+          <div className="field">
+            <h4 className="blue">{props.card.username}</h4>
+          </div>
 
-        <div className="double-field">
-          <h5 className="dark-gray">Telefone</h5>
-          <h5 className="blue">(61) 98272-7924</h5>
-        </div>
-      </body>
-    </ul>
-  );
+          <div className="field">
+            <h5 className="dark-gray">Data de nascimento</h5>
+            <h5 className="blue">{formatDate(props.card.birthdate)}</h5>
+          </div>
+
+          <div className="field">
+            <h5 className="dark-gray">Data de registro</h5>
+            <h5 className="blue">{formatDate(props.card.created_at)}</h5>
+          </div>
+
+          <div className="field">
+            <h5 className="dark-gray">Gênero</h5>
+            <h5 className="blue">{props.card.gender}</h5>
+          </div>
+
+          <div className="field">
+            <h5 className="dark-gray">CPF</h5>
+            <h5 className="blue">{props.card.cpf}</h5>
+          </div>
+
+          <div className="field">
+            <h5 className="dark-gray">RG</h5>
+            <h5 className="blue">{props.card.rg}</h5>
+          </div>
+
+          <div className="field">
+            <h5 className="dark-gray">Celular</h5>
+            <h5 className="blue">{props.card.phone_number}</h5>
+          </div>
+
+          <div className="field">
+            <h5 className="dark-gray">Turma</h5>
+            <h5 className="blue">{props.card.class_number}</h5>
+          </div>
+
+          {props.card.status !== "Active" ? (
+            <button onClick={() => activateMember()}>Ativar</button>
+          ) : (
+            <Fragment></Fragment>
+          )}
+        </body>
+
+        <footer style={backgroundColor} className="card-footer"></footer>
+      </Fragment>
+    );
+  }
+
+  function Card() {
+    return (
+      <div className="card">
+        <IconButton
+          className="icon-more"
+          size="small"
+          aria-label="list"
+          color="primary"
+          onClick={() => setCardFront(!cardFront)}
+        >
+          <MoreVertIcon></MoreVertIcon>
+        </IconButton>
+        {cardFront === true ? CardFront() : CardBack()}
+      </div>
+    );
+  }
+
+  return <Fragment>{Card()}</Fragment>;
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,12 +3,14 @@ import "./styles.css";
 import ViewListIcon from "@material-ui/icons/ViewList";
 import ViewModuleIcon from "@material-ui/icons/ViewModule";
 import { IconButton } from "@material-ui/core";
-import Card from "../../components/Card2/Card";
+import Card from "../../components/Card/Card";
 import CardGrid from "../../components/CardGrid/CardGrid";
+import CardManagerMenu from "../../components/CardManagerMenu/CardManagerMenu";
 import api from "../../services/api";
 
 export default function Dashboard() {
   const [cards, setCards] = useState([]);
+  const [filteredCards, setFilteredCards] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -20,10 +22,24 @@ export default function Dashboard() {
       })
       .then((response) => {
         setCards(response.data);
+        setFilteredCards(response.data);
         setIsLoading(false);
       })
       .catch((err) => console.log("Failed to fetch members. Try again."));
   }, []);
+
+   function filterCards(filter) {
+    console.log('Callback is working. Filtering by', filter);
+
+    if(filter === '*')
+    {
+      setFilteredCards(cards);
+
+      return;
+    }
+
+    setFilteredCards(cards.filter((card) => card.status === filter));
+  }
 
   return (
     <div className="grid-container">
@@ -38,20 +54,8 @@ export default function Dashboard() {
         </ul>
       </aside>
       <main className="main">
-        <ul className="menu">
-          <li className="item dark-gray">
-            <h4>Pendentes</h4>
-          </li>
-          <li className="item dark-gray">
-            <h4>Ativos</h4>
-          </li>
-          <li className="item dark-gray">
-            <h4>Expirados</h4>
-          </li>
-          <li className="item dark-gray">
-            <h4>Todos</h4>
-          </li>
-        </ul>
+        <CardManagerMenu toggleCallback={filterCards} className="menu"/>
+
         <div className="body">
           <h3 className="dark-gray">Cartões pendentes</h3>
 
@@ -69,7 +73,7 @@ export default function Dashboard() {
             <h1>Loading</h1>
           ) : (
             <CardGrid>
-              {cards.map((card) => (
+              {filteredCards.map((card) => (
                 <Card card={card}></Card>
               ))}
             </CardGrid>
